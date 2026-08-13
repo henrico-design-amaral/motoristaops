5c3c59ed9f61bb35fe1ab5f26849867b7ca04dfe
Henrico Amaral <henrico.creator.amaral@gmail.com>
2026-08-12 21:07:05 -0300

    feat: centralize driver profile links

---
+++ src/data/driver-profile.ts
@@ -0,0 +1,11 @@
+export const driverProfile = {
+  name: 'Henrico Amaral',
+  role: 'Motorista particular / aplicativo',
+  location: 'São Paulo e região',
+  phoneDisplay: '(11) 95397-0704',
+  whatsapp: 'https://wa.me/5511953970704?text=Olá%2C%20Henrico.%20Gostaria%20de%20falar%20sobre%20uma%20corrida%20particular.',
+  instagramHandle: '@henrico.driver.amaral',
+  instagram: 'https://www.instagram.com/henrico.driver.amaral/',
+  google: 'https://www.google.com/maps/search/?api=1&query=Henrico%20Amaral%20Motorista%20Particular%20São%20Paulo',
+  slogan: 'Levando pessoas, cuidando de histórias.'
+} as const;
